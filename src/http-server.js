bf4964b533f2a6cb801206318144d29392a11ace
Akash <akashpathadeakol@gmail.com>
2023-07-16 09:22:34 +0530

    https server

--- src/http-server.js
+++ src/http-server.js
@@ -4,14 +4,12 @@ const cors = require('cors');
 const passport = require('passport');
 const { Sequelize } = require('sequelize');
 const db = require('./models/index').sequelize;
-const path = require('path');
-const https = require('https');
-const fs = require('fs');
+
 // const Loan = require('./models/index').loan;
 // const repaymentSchedule = require('./models/index').repaymentSchedule;
 
 //setting up your port
-const PORT = process.env.HTTPS_PORT || 8081
+const PORT = process.env.PORT || 8080
 
 //assigning the variable app to express
 const app = express()
@@ -26,21 +24,10 @@ app.use(express.urlencoded({ extended: false }));
 //     console.log("db has been re sync")
 // })
 
-
-
 app.use(passport.initialize());
 require("./middlewares/jwt")(passport);
 
 require('./routes/index')(app);
 
-
-const httpsServer = https.createServer({
-    key: fs.readFileSync(path.resolve(process.env.SSL_KEY)),
-    cert: fs.readFileSync(path.resolve(process.env.SSL_CERT)),
-}, app);
-
-httpsServer.listen(PORT, () => {
-    console.log(`HTTPS Server running on port ${PORT}`);
-});
 //listening to server connection
-// app.listen(PORT, () => console.log(`Server is running on ${PORT}`))
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`))
